21ab9585d1c68e40246fec2251726a2f9b5fd05a
Vincent Nguyen <vincentn1102@gmail.com>
2024-08-18 12:54:24 +0930

    d

--- prac1.cpp
+++ prac1.cpp
@@ -3,10 +3,9 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
-
 using namespace std;
 
-// Function to convert a base 10 integer to base B
+//Convert base 10 integer to base B
 string convert_to_base_b(long long num, int base) {
     if (num == 0) return "0";
     string result = "";
